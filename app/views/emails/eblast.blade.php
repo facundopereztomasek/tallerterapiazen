00d18997374bd911363050bcf03c8049150fd3e2
= <=>
2015-02-07 21:17:27 -0300

    email taller

---
+++ app/views/emails/eblast.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Inscripci&oacute;n al taller</title>
+	<style>
+		.wrapper{
+			width:650px;
+			background-color:#9b592e;
+		}
+	</style>
+</head>
+<body>
+	<div class="wrapper">
+		<h1>Estas inscripto en el taller!</h1>
+	</div>
+</body>
+</html>
