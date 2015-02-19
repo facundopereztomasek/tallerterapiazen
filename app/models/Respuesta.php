434d6cb76f67642ee2f7b4cd6a1cf0ca1935d388
luco <lucianoperezt@gmail.com>
2015-02-19 15:50:14 -0300

    fix contacto Mayuscula

--- app/models/Respuesta.php
+++ app/models/Respuesta.php
@@ -5,7 +5,7 @@ class Respuesta extends Eloquent {
 	protected $table = 'respuestas';
 
 	public function mensaje(){
-		return $this->belongsTo('mensaje');
+		return $this->belongsTo('Mensaje');
 	}
 
 }
